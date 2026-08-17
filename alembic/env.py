47cd97f8a27b44ae13cd533f8b27e81c3169d243
Hamzah Marie <mariehamzah1@gmail.com>
2026-08-16 23:01:27 -0400

    Add projects table to database

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,13 @@
 from logging.config import fileConfig
 from app.db.database import Base, DATABASE_URL
+from app.models.project import Project
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
